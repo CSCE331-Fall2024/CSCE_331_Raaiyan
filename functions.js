8dcc406f2f84e240e3ff780f07c6efb9c5497c2f
araaiyan <ahmadraaiyan12@gmail.com>
2024-09-04 20:38:20 -0500

    added styles and JS logic for style switcher button

---
+++ functions.js
@@ -0,0 +1,22 @@
+function button1() {
+    if(localStorage.getItem("theme") === "1") {
+        document.getElementById('page_style').setAttribute("href", "style2.css");
+        localStorage.setIten("theme", "2");
+    }
+    else {
+        document.getElementById('page_style').setAttribute("href", "style.css");
+        localStorage.setItem("theme", "1");
+    }
+}
+
+function applySavedTheme() {
+    const savedTheme = localStorage.getItem("theme");
+
+    if (savedTheme === "2") {
+        document.getElementById('page_style').setAttribute("href", "style2.css");
+        localStorage.setItem("theme", "2");
+    } else {
+        document.getElementById('page_style').setAttribute("href", "style.css");
+        localStorage.setItem("theme", "1");
+    }
+}
